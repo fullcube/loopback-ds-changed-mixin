fe972a00078d9ece40d7f4f1bf07cf826c95bc7f
Tom Kirkpatrick <tom@systemseed.com>
2016-04-28 13:01:48 +0200

    Update unit tests

---
+++ test/fixtures/simple-app/common/models/person.js
@@ -0,0 +1,34 @@
+var utils = require('loopback-datasource-juggler/lib/utils');
+var debug = require('debug')('loopback-ds-changed-mixin');
+
+module.exports = function(Person) {
+  // Define a function that should be called when a change is detected.
+  Person.changeName = function(args, cb) {
+    cb = cb || utils.createPromiseCallback();
+    debug('this.changeName() called with %o', args);
+    process.nextTick(function() {
+      cb(null);
+    });
+    return cb.promise;
+  };
+
+  // Define a function that should be called when a change is detected.
+  Person.changeStatus = function(args, cb) {
+    cb = cb || utils.createPromiseCallback();
+    debug('this.changeStatus() called with %o', args);
+    process.nextTick(function() {
+      cb(null);
+    });
+    return cb.promise;
+  };
+
+  // Define a function that should be called when a change is detected.
+  Person.changeAge = function(args, cb) {
+    cb = cb || utils.createPromiseCallback();
+    debug('this.changeAge() called with %o', args);
+    process.nextTick(function() {
+      cb(null);
+    });
+    return cb.promise;
+  };
+};
--- test/test.js
+++ test/test.js
@@ -13,13 +13,7 @@ chai.use(require('sinon-chai'));
 require('mocha-sinon');
 
 // Create a new loopback app.
-var app = loopback();
-
-// Set up promise support for loopback in non-ES6 runtime environment.
-global.Promise = require('bluebird');
-
-// import our Changed mixin.
-require('./')(app);
+var app = require('./fixtures/simple-app/server/server.js');
 
 // Configure datasource
 var dbConnector = null;
@@ -39,78 +33,21 @@ if (MONGODB_URL) {
 
 describe('loopback datasource changed property', function() {
 
-  beforeEach(function(done) {
-
-    // A model with 2 Changed properties.
-    var Person = this.Person = loopback.PersistedModel.extend('person', {
-      name: String,
-      nickname: String,
-      age: Number,
-      status: String,
-    }, {
-      mixins: {
-        Changed: {
-          properties: {
-            name: 'changeName',
-            nickname: 'changeName',
-            age: 'changeAge',
-            status: 'changeStatus'
-          }
-        }
-      }
-    });
-
-    // Define a function that should be called when a change is detected.
-    Person.changeName = function(args, cb) {
-      cb = cb || utils.createPromiseCallback();
-      debug('this.changeName() called with %o', args);
-      process.nextTick(function() {
-        cb(null);
-      });
-      return cb.promise;
-    };
-
-    // Define a function that should be called when a change is detected.
-    Person.changeStatus = function(args, cb) {
-      cb = cb || utils.createPromiseCallback();
-      debug('this.changeStatus() called with %o', args);
-      process.nextTick(function() {
-        cb(null);
-      });
-      return cb.promise;
-    };
-
-    // Define a function that should be called when a change is detected.
-    Person.changeAge = function(args, cb) {
-      cb = cb || utils.createPromiseCallback();
-      debug('this.changeAge() called with %o', args);
-      process.nextTick(function() {
-        cb(null);
-      });
-      return cb.promise;
-    };
-
+  beforeEach(function() {
     // Set up some spies so we can check whether our callbacks have been called.
-    this.spyAge = sinon.spy(Person, 'changeAge');
-    this.spyStatus = sinon.spy(Person, 'changeStatus');
-    this.spyName = sinon.spy(Person, 'changeName');
-
-    Person.attachTo(dbConnector);
-    app.model(Person);
-
-    app.use(loopback.rest());
-    app.set('legacyExplorer', false);
-    done();
+    this.spyAge = this.sinon.spy(app.models.Person, 'changeAge');
+    this.spyStatus = this.sinon.spy(app.models.Person, 'changeStatus');
+    this.spyName = this.sinon.spy(app.models.Person, 'changeName');
   });
 
   lt.beforeEach.withApp(app);
 
   describe('when called internally', function() {
-    lt.beforeEach.givenModel('person',
+    lt.beforeEach.givenModel('Person',
       {title: 'Mr', name:'Joe Blogs', nickname: 'joe', age: 21, status: 'active'}, 'joe');
-    lt.beforeEach.givenModel('person',
+    lt.beforeEach.givenModel('Person',
       {title: 'Mr', name:'Bilbo Baggins', nickname: 'bilbo', age: 99, status: 'active'}, 'bilbo');
-    lt.beforeEach.givenModel('person',
+    lt.beforeEach.givenModel('Person',
       {title: 'Miss', name:'Tina Turner', nickname: 'tina', age: 80, status: 'pending'}, 'tina');
 
     describe('Model.create', function() {
@@ -299,7 +236,7 @@ describe('loopback datasource changed property', function() {
       it('should not run callback if no watched properties are updated', function(done) {
         var self = this;
         this.joe.title = 'Newtitle';
-        this.Person.upsert(this.joe)
+        app.models.Person.upsert(this.joe)
         .then(function(res) {
           expect(self.spyAge).not.to.have.been.called;
           expect(self.spyStatus).not.to.have.been.called;
@@ -312,7 +249,7 @@ describe('loopback datasource changed property', function() {
       it('should not run any callbacks if no the skipChanged option was set to true', function(done) {
         var self = this;
         this.joe.name = 'NewName';
-        this.Person.upsert(this.joe, {skipChanged: true})
+        app.models.Person.upsert(this.joe, {skipChanged: true})
         .then(function(res) {
           expect(self.spyAge).not.to.have.been.called;
           expect(self.spyStatus).not.to.have.been.called;
@@ -326,7 +263,7 @@ describe('loopback datasource changed property', function() {
         var self = this;
         this.joe.name = 'NewName';
         this.joe.status = 'test';
-        this.Person.upsert(this.joe, {skipChanged: 'name'})
+        app.models.Person.upsert(this.joe, {skipChanged: 'name'})
         .then(function(res) {
           expect(self.spyAge).not.to.have.been.called;
           expect(self.spyName).not.to.have.been.called;
@@ -339,7 +276,7 @@ describe('loopback datasource changed property', function() {
       it('should execute 1 callback after updating 1 watched property', function(done) {
         var self = this;
         this.joe.status = 'pending';
-        this.Person.upsert(this.joe)
+        app.models.Person.upsert(this.joe)
           .then(function(res) {
             expect(self.spyAge).not.to.have.been.called;
             expect(self.spyStatus).to.have.been.called;
@@ -353,7 +290,7 @@ describe('loopback datasource changed property', function() {
         var self = this;
         this.joe.status = 'pending';
         this.joe.age = '23';
-        this.Person.upsert(this.joe)
+        app.models.Person.upsert(this.joe)
           .then(function(res) {
             expect(self.spyAge).to.have.been.called;
             expect(self.spyStatus).to.have.been.called;
@@ -367,7 +304,7 @@ describe('loopback datasource changed property', function() {
     describe('Model.updateAll', function() {
       it('should not run callback if no watched properties are updated', function(done) {
         var self = this;
-        this.Person.updateAll(null, {title: 'Newtitle'})
+        app.models.Person.updateAll(null, {title: 'Newtitle'})
         .then(function(res) {
           expect(self.spyAge).not.to.have.been.called;
           expect(self.spyStatus).not.to.have.been.called;
@@ -379,7 +316,7 @@ describe('loopback datasource changed property', function() {
 
       it('should not run any callbacks if no the skipChanged option was set to true', function(done) {
         var self = this;
-        this.Person.updateAll(null, {name: 'NewName'}, {skipChanged: true})
+        app.models.Person.updateAll(null, {name: 'NewName'}, {skipChanged: true})
         .then(function(res) {
           expect(self.spyAge).not.to.have.been.called;
           expect(self.spyStatus).not.to.have.been.called;
@@ -391,7 +328,7 @@ describe('loopback datasource changed property', function() {
 
       it('should not run specific callbacks that are marked to be skipped in skipChanged', function(done) {
         var self = this;
-        this.Person.updateAll(null, {name: 'NewName', status: 'test'}, {skipChanged: 'name'})
+        app.models.Person.updateAll(null, {name: 'NewName', status: 'test'}, {skipChanged: 'name'})
           .then(function(res) {
             expect(self.spyAge).not.to.have.been.called;
             expect(self.spyName).not.to.have.been.called;
@@ -403,7 +340,7 @@ describe('loopback datasource changed property', function() {
 
       it('should execute 1 callback after updating 1 watched propertie on multiple models', function(done) {
         var self = this;
-        this.Person.updateAll(null, {status: 'pending', title: 'Newtitle'})
+        app.models.Person.updateAll(null, {status: 'pending', title: 'Newtitle'})
           .then(function(res) {
             expect(self.spyAge).not.to.have.been.called;
             expect(self.spyStatus).to.have.been.called;
@@ -415,7 +352,7 @@ describe('loopback datasource changed property', function() {
 
       it('should execute 2 callbacks after updating 2 watched properties on multiple models', function(done) {
         var self = this;
-        this.Person.updateAll(null, {status: 'pending', age: '23'})
+        app.models.Person.updateAll(null, {status: 'pending', age: '23'})
           .then(function(res) {
             expect(self.spyAge).to.have.been.called;
             expect(self.spyStatus).to.have.been.called;
